335c883367072bfa53c02535c174956dd45978fb
Ben Carlson <bcarlson336@gmail.com>
2024-01-23 22:45:37 -0700

    Added curl missing case
    Added ZSH install
    
    Added instructions

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+if ! command -v curl &> /dev/null
+then
+    echo "curl could not be found"
+    exit 1
+fi
+
+echo "Installing ZSH"
+sudo apt install zsh
+
+
 echo "Changing the default shell to ZSH"
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
+
+exec zsh
 
 # Install Oh - My - Zsh
 echo "Installing Oh-My-ZSH"
@@ -14,9 +26,9 @@ sed -i 's/ZSH_THEME=.*/ZSH_THEME="eastwood"/' ~/.zshrc
 
 echo "Downloading ZSH plugins"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || exit $?
+sudo git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || exit $?
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting || exit $?
+sudo git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || exit $?
 
 sed -i 's/plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
@@ -26,7 +38,7 @@ source ~/.zshrc && echo "Successfully Setup Zsh Env!" || exit $?
 
 REPO_PATH=~/.con-scripts-repo
 
-git clone https://github.com/carlsondev/convenience-scripts.git $REPO_PATH || exit $?
+git clone --quiet https://github.com/carlsondev/convenience-scripts.git $REPO_PATH || exit $?
 
 echo "Copying scripts to user bin"
 chmod +x $REPO_PATH/scripts/* || exit $?
@@ -39,6 +51,9 @@ echo "Successfully copied custom scripts to user bin"
 
 
 rm -rf $REPO_PATH
+
+
+echo "Successfully setup environment! Run \"chsh -s $(which zsh) && exec zsh && source ~/.zshrc\" to enter"
 
 
 
